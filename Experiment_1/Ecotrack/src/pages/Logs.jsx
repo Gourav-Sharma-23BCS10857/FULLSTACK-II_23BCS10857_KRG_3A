37c13d225d37ee35dbb40b082debafcc3346fb47
Gourav-Sharma-23BCS10857 <23BCS10857@cuchd.in>
2026-01-18 20:46:24 +0530

    Completed Exp 1

--- Experiment_1/Ecotrack/src/pages/Logs.jsx
+++ Experiment_1/Ecotrack/src/pages/Logs.jsx
@@ -6,10 +6,9 @@ const Logs=()=>{
     return(<>
     <div>
          <header style={{padding: "o.5 rem", backgroundColor: "#ff0000", color: "white", textAlign: "center"}}>
-        <h2>high emission causes</h2>
+        <h2>high Carbon Activities {'>'}4 </h2>
         </header>
-        <p>After Filteration above 4</p>
-        <ul style={{padding: "o.5 rem", backgroundColor: "#f70404", color: "white", textAlign: "center"}}>
+        <ul style={{padding: "o.5 rem", backgroundColor: "#f9fdfd", color: "Red", textAlign: "center"}}>
             {
                 highimpactlogs.map(log=>(
                     <li key={log.id}>
@@ -20,17 +19,16 @@ const Logs=()=>{
         </ul>
     </div>
     <div>
-        <header style={{padding: "o.5 rem", color: "green", textAlign: "center"}}>
-        <h2>low emission causes</h2>
+        <header style={{padding: "o.5 rem", color: "white",backgroundColor: "#135f04", textAlign: "center"}}>
+        <h2>low carbon Activities {'<'}4 </h2>
         </header>
-        <p>After Filteration below 4</p>
-        <ul style={{padding: "o.5 rem", backgroundColor: "#04f714", color: "white", textAlign: "center"}}>
+        <ul style={{padding: "o.5 rem", backgroundColor: "#ffffff", color: "green", textAlign: "center"}}>
             {
                 lowimpactlogs.map(log=>(
                     // <li key={log.id} >
                     // {log.activity}= {log.carbon} kg CO2
                     // </li>
-                    <li key={log.id} style={{ color: "blue", fontWeight: "bold" }}>
+                    <li key={log.id} style={{ color: "green", fontWeight: "bold" }}>
                     {log.activity} = {log.carbon} kg CO2
                     </li>
 
